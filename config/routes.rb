7696f1a551dbbbdd4ab2df8f9593efa83317b01f
DiegoCruz02 <a.diegocruz02@gmail.com>
2022-11-14 22:06:07 +0000

    Names for routes added

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Rails.application.routes.draw do
 
   root "movies#index" 
   # Routes for the Movie resource:
-
+# rather than using "/name of path", we will now use the as: :whatever for our code whenever he are handliing paths
   # CREATE
-  post "/movies" =>  "movies#create" 
-  get "/movies/new" => "movies#new" 
+  post "/movies" =>  "movies#create", as: :movies #movies_url and movies_path
+  get "/movies/new" => "movies#new", as: :new_movie #new_movie_url and new_movie_path
           
   # READ
   get "/movies" => "movies#index" 
-  get "/movies/:id" => "movies#show" 
+  get "/movies/:id" => "movies#show", as: :movie #movie_path() (expects an argument to populate :id segment)
   
   # UPDATE
   patch "/movies/:id" => "movies#update" 
-  get "/movies/:id/edit" => "movies#edit" 
+  get "/movies/:id/edit" => "movies#edit", as: :edit_move #edit_movie_path() (expects an argument to populate :id segment)
   
   # DELETE
   delete "/movies/:id" => "movies#destroy" 
