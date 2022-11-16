92e14499997f94e8eaae021ccb80b4fc3e7f9b88
DiegoCruz02 <a.diegocruz02@gmail.com>
2022-11-16 20:01:48 +0000

    Used named route helpers instead of raw URLs

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   
   # UPDATE
   patch "/movies/:id" => "movies#update" 
-  get "/movies/:id/edit" => "movies#edit", as: :edit_move #edit_movie_path() (expects an argument to populate :id segment)
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie #edit_movie_path() (expects an argument to populate :id segment)
   
   # DELETE
   delete "/movies/:id" => "movies#destroy" 
